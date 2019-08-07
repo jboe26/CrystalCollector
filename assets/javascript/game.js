f6febce80db1c1773d428388c5f46a30dd7627fb
jboe26 <joshrboepple@outlook.com>
2019-08-07 17:35:55 -0400

    added JS and updated index

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+var rubyValue = (Math.floor(Math.random() * 11 + 1));
+var diamonValue = (Math.floor(Math.random() * 11 + 1));
+var yellowValue = (Math.floor(Math.random() * 11 + 1));
+var greenValue = (Math.floor(Math.random() * 11 + 1));
+var currentGoal = (Math.floor(Math.random() * 101 + 19));
+$("#current-goal").html(currentGoal);
+$("#total-score").html(totalScore);
+
+function verify (); {
+
+    function reset ();
+
+}
+
+
