26dcc7f99f8b8aa801fc22aa90fc6a97ab55282d
jboe26 <joshrboepple@outlook.com>
2019-08-07 18:31:22 -0400

    updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,30 @@
+$( document ).ready(function() {
+    
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
 
-var rubyValue = (Math.floor(Math.random() * 11 + 1));
-var diamonValue = (Math.floor(Math.random() * 11 + 1));
-var yellowValue = (Math.floor(Math.random() * 11 + 1));
-var greenValue = (Math.floor(Math.random() * 11 + 1));
-var currentGoal = (Math.floor(Math.random() * 101 + 19));
-$("#current-goal").html(currentGoal);
-$("#total-score").html(totalScore);
+var rubyValue = (Math.floor(Math.random() * 11) + 1);
+var diamonValue = (Math.floor(Math.random() * 11) + 1);
+var yellowValue = (Math.floor(Math.random() * 11) + 1);
+var greenValue = (Math.floor(Math.random() * 11) + 1);
+var currentGoal = (Math.floor(Math.random() * 101) + 19);
+$("#goal").html(currentGoal);
+$("#totalScore").html(totalScore);
 
-function verify (); {
-
-    function reset ();
-
+console.log(currentGoal);
+function reset () {
 }
+    reset();
+
+    var rubyValue = (Math.floor(Math.random() * 11 + 1));
+    var diamonValue = (Math.floor(Math.random() * 11 + 1));
+    var yellowValue = (Math.floor(Math.random() * 11 + 1));
+    var greenValue = (Math.floor(Math.random() * 11 + 1));
+    var currentGoal = (Math.floor(Math.random() * 101 + 19));
+    $("#goal").html(currentGoal);
+    $("#totalScore").html(totalScore);
 
 
+
+});
