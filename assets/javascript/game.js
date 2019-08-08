e8728d959977ce1b211e747e816a5d9b70b70d4f
jboe26 <joshrboepple@outlook.com>
2019-08-08 18:23:28 -0400

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,11 +17,11 @@ function reset () {
 }
     reset();
 
-    var rubyValue = (Math.floor(Math.random() * 11 + 1));
-    var diamonValue = (Math.floor(Math.random() * 11 + 1));
-    var yellowValue = (Math.floor(Math.random() * 11 + 1));
-    var greenValue = (Math.floor(Math.random() * 11 + 1));
-    var currentGoal = (Math.floor(Math.random() * 101 + 19));
+    var rubyValue = (Math.floor(Math.random() * 11) + 1);
+    var diamonValue = (Math.floor(Math.random() * 11) + 1);
+    var yellowValue = (Math.floor(Math.random() * 11) + 1);
+    var greenValue = (Math.floor(Math.random() * 11) + 1);
+    var currentGoal = (Math.floor(Math.random() * 101) + 19);
     $("#goal").html(currentGoal);
     $("#totalScore").html(totalScore);
 
