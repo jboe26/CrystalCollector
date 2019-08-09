321765b1c2fb081d5e348148cfa90e04ad9859fa
jboe26 <joshrboepple@outlook.com>
2019-08-09 17:52:34 -0400

    uipdated JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@ var losses = 0;
 var totalScore = 0;
 
 var rubyValue = (Math.floor(Math.random() * 11) + 1);
-var diamonValue = (Math.floor(Math.random() * 11) + 1);
+var diamondValue = (Math.floor(Math.random() * 11) + 1);
 var yellowValue = (Math.floor(Math.random() * 11) + 1);
 var greenValue = (Math.floor(Math.random() * 11) + 1);
 var currentGoal = (Math.floor(Math.random() * 101) + 19);
@@ -17,14 +17,70 @@ function reset () {
 }
     reset();
 
-    var rubyValue = (Math.floor(Math.random() * 11) + 1);
-    var diamonValue = (Math.floor(Math.random() * 11) + 1);
-    var yellowValue = (Math.floor(Math.random() * 11) + 1);
-    var greenValue = (Math.floor(Math.random() * 11) + 1);
-    var currentGoal = (Math.floor(Math.random() * 101) + 19);
+    rubyValue = (Math.floor(Math.random() * 11) + 1);
+    diamondValue = (Math.floor(Math.random() * 11) + 1);
+    yellowValue = (Math.floor(Math.random() * 11) + 1);
+    greenValue = (Math.floor(Math.random() * 11) + 1);
+    currentGoal = (Math.floor(Math.random() * 101) + 19);
     $("#goal").html(currentGoal);
     $("#totalScore").html(totalScore);
 
-
-
+    $(".crystal1").click()
+        totalScore = totalScore + rubyValue
+        $('#totalScore').text(totalScore);
+        if (totalScore === currentGoal) {
+            wins++
+            reset();
+        } else if (totalScore > currentGoal) {
+            losses++
+            reset();
+        }
 });
+
+$(".crystal2").click()
+totalScore = totalScore + diamondValue
+$('#totalScore').text(totalScore);
+if (totalScore === currentGoal) {
+    wins++
+    reset();
+} else if (totalScore > currentGoal) {
+    losses++
+    reset();
+}
+
+$(".crystal3").click()
+totalScore = totalScore + yellowValue
+$('#totalScore').text(totalScore);
+if (totalScore === currentGoal) {
+    wins++
+    reset();
+} else if (totalScore > currentGoal) {
+    losses++
+    reset();
+}
+
+$(".crystal4").click()
+totalScore = totalScore + greenValue
+$('#totalScore').text(totalScore);
+if (totalScore === currentGoal) {
+    wins++
+    reset();
+} else if (totalScore > currentGoal) {
+    losses++
+    reset();
+}
+
+function win(){
+    wins++;
+    $("#wins").text(wins);
+
+    reset();
+}
+
+
+function win(){
+    losses++;
+    $("#losses").text(losses);
+
+    reset();
+}
